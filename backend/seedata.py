a1a47a6e151e13c5ac8371593c11b13fdc668c56
ashrafbel <ashrafmus2@gmail.com>
2024-09-17 13:14:11 -0700

    clean

--- backend/seedata.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-from models import User
-
-
-with app.app_context():
-    users = User.query.all()
-    for user in users:
-        print(f"ID: {user.id}, Email: {user.email}")
